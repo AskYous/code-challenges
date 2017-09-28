8a099387e40f1afab6bcfb346ea7560a2d179eeb
Yousef Shanawany <askyous@gmail.com>
2017-09-28 13:15:47 -0700

    OMG that was so easy

--- solution.js
+++ solution.js
@@ -1,3 +1,36 @@
 function isListPalindrome(l) {
+    var array = [];
+    var current = l;
+    while (current) {
+        array.push(current.value);
+        current = current.next;
+    }
+    for (var i = 0; i < array.length / 2; i++) {
+        if (array[i] !== array[(array.length - 1) - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+var test = [1, 2, 3, 2, 1];
+console.log(isListPalindrome(toLinkedList(test)));
+function toArray(l) {
+    var array = [];
+    var current = l;
+    while (current !== null) {
+        array.push(current.value);
+        current = current.next;
+    }
+    return array;
+}
+function toLinkedList(array) {
+    var l = { value: null, next: null };
+    var temp = l;
+    for (var i = 0; i < array.length - 1; i++) {
+        temp.value = array[i];
+        temp.next = { value: array[i + 1], next: null };
+        temp = temp.next;
+    }
+    return l;
 }
 //# sourceMappingURL=solution.js.map
--- solution.ts
+++ solution.ts
@@ -1,3 +1,41 @@
 function isListPalindrome(l: ListNode<number>): boolean {
+    const array: number[] = [];
+    let current = l;
 
+    while (current) {
+        array.push(current.value);
+        current = current.next;
+    }
+
+    for (let i = 0; i < array.length / 2; i++) {
+        if (array[i] !== array[(array.length - 1) - i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+const test = [1, 2, 3, 2, 1];
+console.log(isListPalindrome(toLinkedList(test)));
+
+function toArray(l: ListNode<number>) {
+    const array = [];
+    let current = l;
+    while (current !== null) {
+        array.push(current.value);
+        current = current.next;
+    }
+    return array;
+}
+function toLinkedList(array: any[]): ListNode<number> {
+    const l = { value: null, next: null };
+    let temp = l;
+
+    for (let i = 0; i < array.length - 1; i++) {
+        temp.value = array[i];
+        temp.next = { value: array[i + 1], next: null };
+        temp = temp.next;
+    }
+    return l;
 }
