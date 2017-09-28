358d93c76e5ab6b94f8c5a53ea453e960588da66
Yousef Shanawany <askyous@gmail.com>
2017-09-27 22:15:16 -0700

    Works for test case one. But needs works.

--- solution.js
+++ solution.js
@@ -1,7 +1,6 @@
-function removeKFromList(l: ListNode<number>, k: number): ListNode<number> {
-    let current = l;
-    let previous: ListNode<number>;
-
+function removeKFromList(l, k) {
+    var current = l;
+    var previous;
     while (current) {
         if (current.value == k) {
             current.value = current.next.value;
@@ -12,27 +11,27 @@ function removeKFromList(l: ListNode<number>, k: number): ListNode<number> {
     console.log(toArray(l));
     return l;
 }
-function toArray(l: ListNode<number>) {
-    const array = [];
-    let current = l;
+function toArray(l) {
+    var array = [];
+    var current = l;
     while (current !== null) {
         array.push(current.value);
         current = current.next;
     }
     return array;
 }
-function toLinkedList(array: any[]): ListNode<number> {
-    const l = { value: null, next: null };
-    let temp = l;
-
-    for (let i = 0; i < array.length - 1; i++) {
+function toLinkedList(array) {
+    var l = { value: null, next: null };
+    var temp = l;
+    for (var i = 0; i < array.length - 1; i++) {
         temp.value = array[i];
         temp.next = { value: array[i + 1], next: null };
         temp = temp.next;
     }
     return l;
 }
-const testCase = toLinkedList([3, 1, 2, 3, 4, 5]);
-const k = 3;
+var testCase = toLinkedList([3, 1, 2, 3, 4, 5]);
+var k = 3;
 console.log(toArray(testCase), k);
 removeKFromList(testCase, 3);
+//# sourceMappingURL=solution.js.map
