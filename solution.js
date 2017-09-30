6fb1fb5c281b2f3fa0bd4ef46e92b4fc630f863b
Yousef Shanawany <askyous@gmail.com>
2017-09-30 14:35:08 -0700

    makeSameLength works again

--- solution.js
+++ solution.js
@@ -2,35 +2,73 @@
 exports.__esModule = true;
 var ListNode = require("./ListNode");
 function addTwoHugeNumbers(a, b) {
+    var DIGIT_LENGTH = 4;
+    var stackA = [];
+    var stackB = [];
     var sum;
-    var answer;
-    var iteratorA = a;
-    var iteratorB = b;
-    if (!a && !b) {
-        return null;
-    }
-    else {
-        if (!a) {
-            return b;
+    var nextA;
+    var nextB;
+    var carry = 0;
+    (function makeSameLength(a2, b2) {
+        var aNextIsNull = (a2.next == null);
+        var bNextIsNull = (b2.next == null);
+        if (aNextIsNull && bNextIsNull) {
+            return;
         }
-        if (!b) {
-            return a;
+        if (aNextIsNull && !bNextIsNull) {
+            a = { value: 0, next: a };
+            makeSameLength(a2, b2.next);
         }
+        else if (!aNextIsNull && bNextIsNull) {
+            b = { value: 0, next: b };
+            makeSameLength(a2.next, b2);
+        }
+        else {
+            makeSameLength(a2.next, b2.next);
+        }
+    })(a, b);
+    (function fillStack() {
+        while (a) {
+            stackA.push(a.value);
+            a = a.next;
+        }
+        while (b) {
+            stackB.push(b.value);
+            b = b.next;
+        }
+    })();
+    nextA = stackA.pop();
+    nextB = stackB.pop();
+    while (nextA != null) {
+        var tempSum = void 0;
+        var leftNumber = void 0;
+        var rightNumber = void 0;
+        tempSum = String(nextA + nextB + carry);
+        if (tempSum.length > DIGIT_LENGTH) {
+            carry = +tempSum.slice(0, 1);
+            if (sum == null) {
+                sum = new ListNode(+tempSum.slice(1));
+            }
+            else {
+                sum = {
+                    value: +tempSum.slice(1),
+                    next: sum
+                };
+            }
+        }
+        else {
+            carry = 0;
+        }
+        nextA = stackA.pop();
+        nextB = stackB.pop();
     }
-    sum = String(a.value + b.value);
-    if (sum.length > 4) {
-        var leftNum = Number(sum.slice(0, 1));
-        var rightNum = Number(sum.slice(1));
-        answer = { value: leftNum, next: new ListNode(rightNum) };
-    }
-    else {
-        answer = { value: Number(sum), next: addTwoHugeNumbers(a.next, b.next) };
-    }
-    return answer;
+    return carry > 0 ? { value: carry, next: sum } : sum;
 }
 var LinkedListHelper = require("./LinkedListHelper");
-var a = [9876, 5432, 1999];
-var b = [1, 8001];
-var testResult = addTwoHugeNumbers(LinkedListHelper.toLinkedList(a), LinkedListHelper.toLinkedList(b));
-console.log(LinkedListHelper.toArray(testResult));
+(function test() {
+    var a = [9876, 5432, 1999];
+    var b = [1, 8001];
+    var testResult = addTwoHugeNumbers(LinkedListHelper.toLinkedList(a), LinkedListHelper.toLinkedList(b));
+    console.log(LinkedListHelper.toArray(testResult));
+})();
 //# sourceMappingURL=solution.js.map
--- solution.ts
+++ solution.ts
@@ -1,32 +1,76 @@
 import ListNode = require('./ListNode');
 function addTwoHugeNumbers(a: ListNode<number>, b: ListNode<number>): ListNode<number> {
-    let sum: string;
-    let answer: ListNode<number>;
-    let iteratorA: ListNode<number> = a;
-    let iteratorB: ListNode<number> = b;
+    const DIGIT_LENGTH = 4;
+    const stackA: number[] = [];
+    const stackB: number[] = [];
 
-    if (!a && !b) {
-        return null;
-    } else {
-        if (!a) { return b; }
-        if (!b) { return a; }
+    let sum: ListNode<number>;
+    let nextA: number;
+    let nextB: number;
+    let carry: number = 0;
+
+    (function makeSameLength(a2: ListNode<number>, b2: ListNode<number>): void {
+        const aNextIsNull: boolean = (a2.next == null);
+        const bNextIsNull: boolean = (b2.next == null);
+
+        if (aNextIsNull && bNextIsNull) { return; }
+        if (aNextIsNull && !bNextIsNull) {
+            a = { value: 0, next: a };
+            makeSameLength(a2, b2.next);
+        }
+        else if (!aNextIsNull && bNextIsNull) {
+            b = { value: 0, next: b };
+            makeSameLength(a2.next, b2);
+        } else {
+            makeSameLength(a2.next, b2.next);
+        }
+    })(a, b);
+    (function fillStack() {
+        while (a) {
+            stackA.push(a.value);
+            a = a.next;
+        }
+        while (b) {
+            stackB.push(b.value);
+            b = b.next;
+        }
+    })();
+
+    nextA = stackA.pop();
+    nextB = stackB.pop();
+    while (nextA != null) {
+        let tempSum: string;
+        let leftNumber: string;
+        let rightNumber: string;
+
+        tempSum = String(nextA + nextB + carry);
+
+        if (tempSum.length > DIGIT_LENGTH) {
+            carry = +tempSum.slice(0, 1);
+            if (sum == null) {
+                sum = new ListNode<number>(+tempSum.slice(1));
+            } else {
+                sum = {
+                    value: +tempSum.slice(1),
+                    next: sum
+                };
+            }
+        } else {
+            carry = 0;
+        }
+
+        nextA = stackA.pop();
+        nextB = stackB.pop();
     }
-
-    sum = String(a.value + b.value);
-
-    if (sum.length > 4) {
-        const leftNum: number = Number(sum.slice(0, 1));
-        const rightNum: number = Number(sum.slice(1))
-        answer = { value: leftNum, next: new ListNode<number>(rightNum) }
-    } else {
-        answer = { value: Number(sum), next: addTwoHugeNumbers(a.next, b.next) }
-    }
-    return answer;
+    return carry > 0 ? { value: carry, next: sum } : sum;
 }
 
 import LinkedListHelper = require('./LinkedListHelper');
-const a = [9876, 5432, 1999];
-const b = [1, 8001];
+(function test() {
+    const a = [9876, 5432, 1999];
+    const b = [1, 8001];
 
-const testResult = addTwoHugeNumbers(LinkedListHelper.toLinkedList(a), LinkedListHelper.toLinkedList(b));
-console.log(LinkedListHelper.toArray(testResult));
+    const testResult = addTwoHugeNumbers(LinkedListHelper.toLinkedList(a), LinkedListHelper.toLinkedList(b));
+    console.log(LinkedListHelper.toArray(testResult));
+})();
+
