ff026998916ee0b0c63aef1d1fca2be6532f16c8
Yousef Shanawany <askyous@gmail.com>
2017-10-01 16:49:16 -0700

    Done

--- solution.js
+++ solution.js
@@ -1,96 +1,28 @@
 "use strict";
 exports.__esModule = true;
-var ListNode_1 = require("./ListNode");
-function addTwoHugeNumbers(a, b) {
-    var DIGIT_LENGTH = 4;
-    var stackA = [];
-    var stackB = [];
-    var sum;
-    var nextA;
-    var nextB;
-    var carry = 0;
-    (function makeSameLength(a2, b2) {
-        var aNextIsNull = (a2.next == null);
-        var bNextIsNull = (b2.next == null);
-        if (aNextIsNull && bNextIsNull) {
-            return;
-        }
-        if (aNextIsNull && !bNextIsNull) {
-            var temp = new ListNode_1.ListNode(a.value);
-            temp.next = a.next;
-            a = new ListNode_1.ListNode(0);
-            a.next = temp;
-            makeSameLength(a2, b2.next);
-        }
-        else if (!aNextIsNull && bNextIsNull) {
-            var temp = new ListNode_1.ListNode(b.value);
-            temp.next = b.next;
-            b = new ListNode_1.ListNode(0);
-            b.next = temp;
-            makeSameLength(a2.next, b2);
-        }
-        else {
-            makeSameLength(a2.next, b2.next);
-        }
-    })(a, b);
-    (function fillStack() {
-        while (a) {
-            stackA.push(a.value);
-            a = a.next;
-        }
-        while (b) {
-            stackB.push(b.value);
-            b = b.next;
-        }
-    })();
-    nextA = stackA.pop();
-    nextB = stackB.pop();
-    while (nextA != null) {
-        var tempSum = void 0;
-        var leftNumber = void 0;
-        var rightNumber = void 0;
-        tempSum = String(nextA + nextB + carry);
-        if (tempSum.length > DIGIT_LENGTH) {
-            carry = +tempSum.slice(0, 1);
-            if (sum == null) {
-                sum = new ListNode_1.ListNode(+tempSum.slice(1));
-            }
-            else {
-                var temp = new ListNode_1.ListNode(sum.value);
-                temp.next = sum.next;
-                sum = new ListNode_1.ListNode(+tempSum.slice(1));
-                sum.next = temp;
-            }
-        }
-        else {
-            carry = 0;
-            if (sum == null) {
-                sum = new ListNode_1.ListNode(+tempSum);
-            }
-            else {
-                var temp = new ListNode_1.ListNode(sum.value);
-                temp.next = sum.next;
-                sum = new ListNode_1.ListNode(+tempSum);
-                sum.next = temp;
-            }
-        }
-        nextA = stackA.pop();
-        nextB = stackB.pop();
+function mergeTwoLinkedLists(a, b) {
+    var sorted;
+    if (!a !== !b) {
+        return a || b;
+    } // if a or b is null, return the not-null one.
+    if (!a && !b) {
+        return null;
     }
-    if (carry > 0) {
-        var answer = new ListNode_1.ListNode(carry);
-        answer.next = sum;
-        return answer;
+    if (a.value > b.value) {
+        sorted = b;
+        sorted.next = mergeTwoLinkedLists(a, b.next);
     }
     else {
-        return sum;
+        sorted = a;
+        sorted.next = mergeTwoLinkedLists(a.next, b);
     }
+    return sorted;
 }
 var LinkedListHelper = require("./LinkedListHelper");
 (function test() {
-    var a = [123, 4, 5];
-    var b = [100, 100, 100];
-    var testResult = addTwoHugeNumbers(LinkedListHelper.toLinkedList(a), LinkedListHelper.toLinkedList(b));
+    var a = [1, 3];
+    var b = [2, 4];
+    var testResult = mergeTwoLinkedLists(LinkedListHelper.toLinkedList(a), LinkedListHelper.toLinkedList(b));
     console.log(LinkedListHelper.toArray(testResult));
 })();
 //# sourceMappingURL=solution.js.map
--- solution.ts
+++ solution.ts
@@ -1,33 +1,24 @@
-import { ListNode } from './ListNode';
 function mergeTwoLinkedLists(a: ListNode<number>, b: ListNode<number>): ListNode<number> {
-    (function makeSameLength(a2: ListNode<number>, b2: ListNode<number>): void {
-        const aNextIsNull: boolean = (a2.next == null);
-        const bNextIsNull: boolean = (b2.next == null);
+    let sorted: ListNode<number>;
+    if (!a !== !b) { return a || b } // if a or b is null, return the not-null one.
+    if (!a && !b) { return null; }
 
-        if (aNextIsNull && bNextIsNull) { return; }
-        if (aNextIsNull && !bNextIsNull) {
-            const temp: ListNode<number> = new ListNode<number>(a.value);
-            temp.next = a.next;
-            a = new ListNode<number>(null);
-            a.next = temp;
-            makeSameLength(a2, b2.next);
-        }
-        else if (!aNextIsNull && bNextIsNull) {
-            const temp: ListNode<number> = new ListNode<number>(b.value);
-            temp.next = b.next;
-            b = new ListNode<number>(null);
-            b.next = temp;
-            makeSameLength(a2.next, b2);
-        } else {
-            makeSameLength(a2.next, b2.next);
-        }
-    })(a, b);
+    if (a.value > b.value) {
+        sorted = b;
+        sorted.next = mergeTwoLinkedLists(a, b.next)
+    } else {
+        sorted = a;
+        sorted.next = mergeTwoLinkedLists(a.next, b)
+    }
+
+    return sorted;
 }
 
+import { ListNode } from './ListNode';
 import LinkedListHelper = require('./LinkedListHelper');
 (function test() {
-    const a = [1, 2, 3];
-    const b = [4, 5, 6];
+    const a = [1, 3];
+    const b = [2, 4];
 
     const testResult = mergeTwoLinkedLists(LinkedListHelper.toLinkedList(a), LinkedListHelper.toLinkedList(b));
     console.log(LinkedListHelper.toArray(testResult));
