34fbf26e46d707705b3f192407dc6c23959e209d
Yousef Shanawany <askyous@gmail.com>
2017-10-06 21:34:13 -0700

    Easy

--- old/groupingDishes/solution.ts
+++ old/groupingDishes/solution.ts
@@ -1,39 +1,46 @@
-function groupingDishes(dishes) {
+function groupingDishes(dishes: string[][]): string[][] {
     // read all ingrediants and store in OBJECT if not already stored
     // for each ingrediant, store the name of the dishes that have it (if more than two!)
-    var ingrediants = {};
-    var sorted = [];
+
+    const ingrediants: object = {};
+    let sorted: string[][] = [];
+
     // create the ingrediants object
-    for (var di = 0; di < dishes.length; di++) {
-        var d = dishes[di];
-        for (var ii = 1; ii < d.length; ii++) {
-            var ingrediant = d[ii];
+    for (let di = 0; di < dishes.length; di++) {
+        const d = dishes[di];
+
+        for (let ii = 1; ii < d.length; ii++) {
+            const ingrediant = d[ii];
+
             // if that ingrediant wasn't added to the list, create that ingrediant
             if (!ingrediants[ingrediant]) {
                 ingrediants[ingrediant] = [d[0]];
-            }
-            else {
-                ingrediants[ingrediant].push(d[0]);
+            } else {
+                ingrediants[ingrediant].push(d[0])
             }
         }
     }
+
+
+
     // remove the ingrediants that have 1 item
-    for (var ingName in ingrediants) {
+    for (let ingName in ingrediants) {
         if (ingrediants.hasOwnProperty(ingName)) {
             if (ingrediants[ingName].length < 2) {
                 delete ingrediants[ingName];
-            }
-            else {
+            } else {
                 ingrediants[ingName] = ingrediants[ingName].sort();
                 sorted.push([ingName].concat(ingrediants[ingName]));
             }
         }
     }
-    return sorted.sort(function (a, b) { return a[0] > b[0] ? 1 : -1; });
+
+    return sorted.sort((a, b) => a[0] > b[0] ? 1 : -1)
 }
-var dishes = [["Salad", "Tomato", "Cucumber", "Salad", "Sauce"],
+
+const dishes =
+    [["Salad", "Tomato", "Cucumber", "Salad", "Sauce"],
     ["Pizza", "Tomato", "Sausage", "Sauce", "Dough"],
     ["Quesadilla", "Chicken", "Cheese", "Sauce"],
     ["Sandwich", "Salad", "Bread", "Tomato", "Cheese"]];
 console.log(groupingDishes(dishes));
-//# sourceMappingURL=solution.js.map
