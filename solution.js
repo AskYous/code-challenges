6b709042262499ecd3d717eff6babe02a1a87696
Yousef Shanawany <askyous@gmail.com>
2018-02-22 20:21:43 -0800

    Better but still too slow

--- solution.js
+++ solution.js
@@ -1,29 +1,13 @@
 function findProfession(level, pos) {
-    const tree = new Tree(true, 1, level);
-    const absPos = Math.pow(2, level - 1) + (pos - 2);
-    const answer = findNthChildAtLevel(tree, absPos);
-    return answer ? "Engineer" : "Doctor";
-}
-
-function findNthChildAtLevel(tree, n, i = 0) {
-    if (tree == null) return;
-    if (i == n) {
-        return tree.value;
-    }
-    const left = findNthChildAtLevel(tree.leftChild, n, (2 * i) + 1);
-
-    if (left == null) return findNthChildAtLevel(tree.rightChild, n, (2 * i) + 2);
-    else return left;
-}
-
-class Tree {
-    constructor(rootValue, row = 1, max) {
-        this.value = rootValue;
-        if (row < max) {
-            this.leftChild = new Tree(rootValue, row + 1, max);
-            this.rightChild = new Tree(!rootValue, row + 1, max);
+    let str = 'E';
+    for (let i = 1; i < level; i++) {
+        let nextLevel = '';
+        for (let j = 0; j < str.length; j++) {
+            nextLevel += str[j] == 'E' ? 'ED' : 'DE';
         }
+        str = nextLevel;
     }
+    return str[pos - 1] == 'D' ? 'Doctor' : 'Engineer';
 }
 
 module.exports = findProfession;
