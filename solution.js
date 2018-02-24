823506ed2d7e6f48a70fd9c8d9b01248ad61accb
Yousef Shanawany <askyous@gmail.com>
2018-02-23 16:31:05 -0800

    Done!

--- solution.js
+++ solution.js
@@ -1,13 +1,15 @@
 function findProfession(level, pos) {
-    let str = 'E';
-    for (let i = 1; i < level; i++) {
-        let nextLevel = '';
-        for (let j = 0; j < str.length; j++) {
-            nextLevel += str[j] == 'E' ? 'ED' : 'DE';
-        }
-        str = nextLevel;
-    }
-    return str[pos - 1] == 'D' ? 'Doctor' : 'Engineer';
+  const absPos = Math.pow(2, level - 1) + pos - 1;
+  const parents = [];
+  let n = true;
+  for (let i = absPos; i > 0; i = Math.floor(i / 2)) {
+    parents.push(i);
+  }
+  parents.reverse();
+  for (let p of parents) {
+    n = p % 2 !== 0 ? !n : n;
+  }
+  return n ? 'Doctor' : 'Engineer';
 }
 
 module.exports = findProfession;
--- solution.spec.js
+++ solution.spec.js
@@ -1,17 +1,17 @@
-const solution = require("./solution");
+const solution = require('./solution');
 
-let expected = "Doctor";
+let expected = 'Doctor';
 let actual = solution(3, 3);
-console.assert(expected == actual);
+console.assert(expected === actual);
 
-expected = "Doctor";
+expected = 'Doctor';
 actual = solution(4, 2);
-console.assert(expected == actual);
+console.assert(expected === actual);
 
-expected = "Engineer";
+expected = 'Engineer';
 actual = solution(1, 1);
-console.assert(expected == actual);
+console.assert(expected === actual);
 
-expected = "Engineer";
+expected = 'Engineer';
 actual = solution(25, 16777216);
-console.assert(expected == actual);
+console.assert(expected === actual);
